d0bb4ae76d1a55845d2a495d677aa4ad608ef0f1
Sampath-Lokuge <sugeesampathlokuge@gmail.com>
2019-07-05 04:51:03 +0530

    tslint

--- src/app/services/utilities/network-state-service.ts
+++ src/app/services/utilities/network-state-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Network } from "@ionic-native/network/ngx";
+import { Network } from '@ionic-native/network/ngx';
 import { ShowToastService } from './show-toast.service';
 
 @Injectable({
@@ -16,13 +16,13 @@ export class NetworkStateService {
   WatchConnection() {
     if (this.connectSubscription$) { this.connectSubscription$.unsubscribe(); }
     this.connectSubscription$ = this.network.onDisconnect().subscribe(() => {
-      this.showToastService.showNetworkStateErrorToast("Your internet seems to be down! Please check your network settings!");
+      this.showToastService.showNetworkStateErrorToast('Your internet seems to be down! Please check your network settings!');
       if (this.connectSubscription$) { this.connectSubscription$.unsubscribe(); }
       this.connectSubscription$ = this.network.onConnect().subscribe(() => {
         setTimeout(() => {
           this.showToastService.toast.dismiss();
-          if (this.network.type === "wifi" || this.network.type === "4g" || this.network.type === "3g" || this.network.type === "2g" || this.network.type === "cellular" || this.network.type === "none") {
-            this.showToastService.showNetworkStateSuccessToast("Internet connection available!");
+          if (this.network.type === 'wifi' || this.network.type === '4g' || this.network.type === '3g' || this.network.type === '2g' || this.network.type === 'cellular' || this.network.type === 'none') {
+            this.showToastService.showNetworkStateSuccessToast('Internet connection available!');
             this.WatchConnection();
           }
         }, 3000);
